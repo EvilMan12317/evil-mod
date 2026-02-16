ac9711a003dafc9e520807ab2deaf84a915d3c1c
EvilMan12317 <godofredoalfredo69@gmail.com>
2026-02-16 16:30:38 -0300

    Delete scripts/main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-Events.on(ClientLoadEvent, () => {
-  Core.bundle.debug("Nigger");
-  Vars.content.each(c => {
-    if (!(c instanceof UnlockableContent)) return;
-    c.localizedName = "Nigger";
-    c.description = "Nigger";
-    c.details = "Nigger";
-  });
-});
